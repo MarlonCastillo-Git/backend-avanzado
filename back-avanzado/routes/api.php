e7778d1ab5c941ad336b56d200e9a23ca1f48baf
huguinhosolari <hugol.barrientos@gmail.com>
2026-09-01 20:06:57 -0600

    add register, login & me functions

--- back-avanzado/routes/api.php
+++ back-avanzado/routes/api.php
@@ -3,6 +3,7 @@
 use App\Models\Category;
 use App\Models\Product;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\OrderController;
@@ -18,3 +19,11 @@ Route::get('/user', function (Request $request) {
 Route::apiResource('products', ProductController::class);
 
 Route::apiResource('orders', OrderController::class);
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::get('/me', [AuthController::class, 'me']);
+
+
+Route::get('/meByEmail', [AuthController::class, 'meByEmail']);
